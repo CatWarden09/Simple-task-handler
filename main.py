d79c35e5bf022f10d231aed5fef7d56211b4601d
CatWarden09 <CatWarden09@proton.me>
2025-11-24 19:57:30 +0400

    Updated program version in main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pywinauto as pw
 from dotenv import load_dotenv
 from pywinauto import timings
 
-VERSION = "0.2.2"
+VERSION = "0.2.3"
 
 if getattr(sys, "frozen", False):
     script_dir = os.path.dirname(sys.executable)  # for exe version
